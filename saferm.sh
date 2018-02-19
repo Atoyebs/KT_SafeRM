5d6ce807cac4dc17aadc7c1bf560766d660f9c60
Ini Atoyebi <ato_no2@hotmail.co.uk>
2018-02-19 03:02:48 +0100

    finally gotten the recursive safe remove algorithm to work in line with the way rm works :-)

--- saferm.sh
+++ saferm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pointOfExitArray=()
 homeDir="$HOME"
 trashSafermDirName=".Trash_saferm"
 trashSafermPath="$homeDir/$trashSafermDirName"
@@ -8,16 +7,15 @@ srmTrue="0"
 srmFalse="1"
 rootDir=$1
 currentDir="$rootDir"
+treeDepth=-1
+treeDepthIterationArray[0]=1
 
 #adding function for checking if directory exists at path
-
 doesDirectoryExist(){
-
     if [ ! -d "$2/$1" ]; then
         mkdir "$2/$1"
         echo "Created Directory $2/$1 because it didn't previously exist"
     fi
-
 }
 
 
@@ -27,7 +25,6 @@ createTrashSafeRMDir(){
 
 isResponseYes(){
 
-#	response=$(echo "$1" | awk '{print substr($0,0,1)}')
 	response="$1"
 
 	#if the first letter of the response is upper or lower case Y
@@ -48,7 +45,7 @@ shouldEnterDirectory(){
 	then
 
 		#ask them if they want to enter the directory (if it is a directory)
-		read -p "do you want to enter the directory, $1? " response
+		read -p "examine files in directory $1? " response
 
 		#if the first letter of the response is upper or lower case Y
 		if [[ ${response:0:1} == 'y' || ${response:0:1} == 'Y' ]]
@@ -68,12 +65,33 @@ shouldEnterDirectory(){
 }
 
 
-#create a function to:
-#	check whether the delete iteration you are on is the final file/directory in the directory: call function isFinalIteration
-# if there are directories then it is the last directory
-# if there are NO directories then it is the last file
+finalDeleteOfDirectory(){
 
-#create a counter to increment the count
+	read -p "remove $1? " response
+
+	isResponseYes $response
+
+	#if the user resopnds yes to the directory removal prompt
+	if [ $? -eq $srmTrue ]
+	then
+
+		treeDepth=$((treeDepth - 1))
+
+		directoryContentsCount=$(ls -l "$1" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' | wc -l | xargs)
+
+		#if the count of the items in the directory is GREATER THAN 0 then
+		if [ $directoryContentsCount -gt 0 ]
+		then
+			echo "saferm: $1: Directory not empty"
+		else
+			#otherwise move the file at the specific path to the safe_rm recycle bin
+			mv "$1" $trashSafermPath
+		fi
+
+	fi
+
+}
+
 
 
 recursivelyDeleteContentsOfDirectory(){
@@ -83,62 +101,69 @@ recursivelyDeleteContentsOfDirectory(){
 	#if the user has chosen to enter the directory
 	if [ $? -eq $srmTrue ]
 	then
-
 		#currentDir is whatever the first parameter is
 		currentDir="$1"
-
-		#count how many items are inside the directory
-
-		#LIST of files within the current directory
-		currentDirFileListing=$(ls -l "$currentDir" | grep -v '^d' | sed -n '1!p' | awk -F " " '{print $NF}')
-
-		#COUNT of files within the current directory
-		currentDirFileCount=$(ls -l "$currentDir" | grep -v '^d' | sed -n '1!p' | awk -F " " '{print $NF}'| wc -l | xargs)
-
-		#LIST of directories within the current directory
-		currentDirDirListing=$(ls -l "$currentDir" | grep '^d' | awk -F " " '{print $NF}')
-
-		#COUNT of directories within the current directory
-		currentDirDirListingCount=$(ls -l "$currentDir" | grep '^d' | awk -F " " '{print $NF}' | wc -l | xargs)
-
-		#get the total number of files and directories (items) in this directory
-		let totalListing=$currentDirFileCount+$currentDirDirListingCount
+		treeDepth=$((treeDepth + 1))
+		#get total list of files and directories (IN THAT ORDER)
+		totalItemListing=$(ls -l "$currentDir" | sort -k1,1 | awk -F " " '{print $NF}' | sed -e '$ d' )
+		#set the iterations remaining in the depth array for this depth
+		treeDepthIterationArray[$treeDepth]=$(echo "$totalItemListing" | wc -l | xargs)
 
 		#loop through every item asking if the user wants to delete it
-		for item in $currentDirFileListing
+		for item in $totalItemListing
 		do
 
-			#ask them if they want to enter the directory (if it is a directory)
-			read -p "do you want to remove file $item? " response
-
-			#is the user response yes or no
-			isResponseYes $response
-
-			#if their response is yes
-			if [ $? -eq $srmTrue ]
+			#if the current item in the iteration is a FILE
+			if [ -f "$currentDir/$item" ]
 			then
-				let totalListing--
+
+				read -p "remove $item? " response
+				isResponseYes $response
+
+				#if the user resopnds yes to the file removal prompt
+				if [ $? -eq $srmTrue ]
+				then
+					mv "$currentDir/$item" $trashSafermPath
+					totalItemsRemaining=$((totalItemsRemaining - 1))
+				fi
+
+			fi
+
+#======================================================================================================
+
+			#if the current item in the iteration is a DIRECTORY
+			if [ -d "$currentDir/$item" ]
+			then
+				recursivelyDeleteContentsOfDirectory "$currentDir/$item"
+			fi
+
+			#reduce the number of iterations remaining by 1
+			treeDepthIterationArray[$treeDepth]=$(( ${treeDepthIterationArray[$treeDepth]} - 1 ))
+
+			#it the number of iterations remaining are equal to zero
+			if [ ${treeDepthIterationArray[$treeDepth]} -eq 0 ]
+			then
+				#name of the directory being worked on (Parent directory)
+				workingDir=$(basename "$currentDir")
+				#PATH of the directory being worked on
+				currentDir=$(dirname "$currentDir")
+				#this function prompts the final deletion of a directory and handles its response
+				finalDeleteOfDirectory "$currentDir/$workingDir"
 			fi
 
 		done
 
-		#loop through all directories
-		for item in $currentDirDirListing
-		do
-			recursivelyDeleteContentsOfDirectory "$currentDir/$item"
-			let totalListing--
-		done
-
+		#when all iterations are complete ask if you want to delete the root folder you started with
+		if [[ $treeDepth -eq 0 && ${treeDepthIterationArray[$treeDepth]} -eq 1 ]]
+		then
+			finalDeleteOfDirectory "$rootDir"
+		fi
 
 	fi
 
-
 }
-
 
 #code to Create the trash directory if it doesn't already exist
 createTrashSafeRMDir
-
-
 
 recursivelyDeleteContentsOfDirectory $1
